0d61311c176044e0bd0959eae56993cef19cc144
Ryan Rhodes <rrhodes.webdev@gmail.com>
2020-04-26 19:45:16 -0400

    refactored to burger menu

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,4 +1,4 @@
-import React from "react"
+import React, { useState } from "react"
 import { Link } from "gatsby"
 import styled from "styled-components"
 import logo from "../../images/jesserhodes-logo.png"
@@ -11,25 +11,37 @@ const HeaderWrapper = styled.header`
 `
 
 const Logo = styled.img`
-  max-width: 150px;
+  max-width: 200px;
 `
 
 const LinksWrapper = styled.nav`
+  background-color: rgba(26, 23, 19, 0.75);
+  height: 100vh;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 100%;
+`
+
+const LinkContainer = styled.div`
   align-items: center;
+  background-color: #ffffff;
   display: flex;
-  height: 100%;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  position: absolute;
+  right: 0;
+  top: 0;
+  width: 500px;
   li {
-    align-items: center;
-    display: flex;
-    font-size: 16px;
+    font-size: 42px;
     font-weight: 300;
-    height: 100%;
     list-style: none;
-    padding: 0 20px;
+    padding: 20px;
     a {
       color: #1a1713;
       text-decoration: none;
-      text-transform: uppercase;
       &:hover {
         color: #b18e72;
       }
@@ -38,15 +50,20 @@ const LinksWrapper = styled.nav`
       }
     }
   }
+  @media screen and (max-width: 768px) {
+    width: 100%;
+  }
 `
 
-const Header = () => {
+const Links = () => {
   return (
-    <HeaderWrapper>
-      <Link to="/">
-        <Logo src={logo} alt="Jesse Rhodes Logo" />
-      </Link>
-      <LinksWrapper>
+    <LinksWrapper>
+      <LinkContainer>
+        <li>
+          <Link to="/" activeClassName="active-link">
+            Home
+          </Link>
+        </li>
         <li>
           <Link to="/about" activeClassName="active-link">
             About
@@ -67,7 +84,35 @@ const Header = () => {
             Contact
           </Link>
         </li>
-      </LinksWrapper>
+      </LinkContainer>
+    </LinksWrapper>
+  )
+}
+
+const Header = () => {
+  const [menuActive, setMenuActive] = useState(false)
+
+  const onBurgerClick = e => {
+    const burger = document.getElementsByClassName("hamburger")
+    burger[0].classList.toggle("is-active")
+    setMenuActive(!menuActive)
+  }
+
+  return (
+    <HeaderWrapper>
+      <Link to="/">
+        <Logo src={logo} alt="Jesse Rhodes Logo" />
+      </Link>
+      <button
+        className="hamburger hamburger--collapse"
+        type="button"
+        onClick={onBurgerClick}
+      >
+        <span className="hamburger-box">
+          <span className="hamburger-inner"></span>
+        </span>
+      </button>
+      {menuActive && <Links />}
     </HeaderWrapper>
   )
 }
