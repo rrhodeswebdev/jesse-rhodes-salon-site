021e2c27c308ff06e19b1c04d3162c8a4659632c
Ryan Rhodes <ryan@ryanrhodes.io>
2020-06-29 21:35:24 -0400

    finalized v1 styles (#9)

--- src/components/content/Testimonial.js
+++ src/components/content/Testimonial.js
@@ -4,28 +4,37 @@ import styled from 'styled-components';
 
 const TestimonialTextWrapper = styled.div`
 	outline: none;
-	max-width: 400px;
+	max-width: 600px;
 	margin: 0 auto;
 `;
 
 const TestimonialText = styled.p`
-	font-size: 18px;
+	font-size: 24px;
+	font-weight: 300;
+	text-align: center;
+`;
+
+const TestimonialPerson = styled.p`
+	font-size: 24px;
+	font-weight: 300;
 	text-align: center;
 `;
 
 const Testimonial = props => {
-	const { quote } = props;
+	const { quote, person } = props;
 	return (
 		<TestimonialTextWrapper>
 			<TestimonialText>
 				<em>{`"${quote}"`}</em>
 			</TestimonialText>
+			<TestimonialPerson>{`- ${person}`}</TestimonialPerson>
 		</TestimonialTextWrapper>
 	);
 };
 
 Testimonial.propTypes = {
 	quote: PropTypes.string.isRequired,
+	person: PropTypes.string.isRequired,
 };
 
 export default Testimonial;
