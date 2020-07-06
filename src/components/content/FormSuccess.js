ccc40fbde100941600a89f2312053dfb25723b1d
rrhodeswebdev <ryan@ryanrhodes.io>
2020-07-05 23:01:08 -0400

    updated contact page and connected hubspot

---
+++ src/components/content/FormSuccess.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+
+const Banner = styled.div`
+	background-color: #1a1713;
+	margin-bottom: 20px;
+	padding: 20px;
+`;
+
+const Text = styled.span`
+	color: #ffffff;
+	font-size: 16px;
+	font-weight: 300;
+`;
+
+const FormSuccess = props => {
+	const { message } = props;
+	return (
+		<Banner>
+			<Text>{message}</Text>
+		</Banner>
+	);
+};
+
+FormSuccess.propTypes = {
+	message: PropTypes.string.isRequired,
+};
+
+export default FormSuccess;
---
+++ src/components/elements/Divider.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const Divider = styled.div`
+	height: ${props => (props.height ? props.height : '2px')};
+	width: ${props => (props.width ? props.width : '50%')};
+	background-color: ${props => (props.bg ? props.bg : '#b18e72')};
+`;
