033e910dc800174ad272c8ffdea970925d13eb9a
Ryan Rhodes <ryan@ryanrhodes.io>
2020-07-10 21:39:06 -0400

    updated button component style (#14)

--- src/components/elements/Button.js
+++ src/components/elements/Button.js
@@ -5,14 +5,16 @@ export const Button = styled.button`
 	color: ${props => (props.color ? props.color : '#ffffff')};
 	outline: none;
 	border: none;
-	padding: 10px 20px;
+	padding: 15px 20px;
 	margin-right: 20px;
 	margin-left: 20px;
 	cursor: pointer;
-	min-width: 200px;
+	min-width: 250px;
 	font-weight: 300;
 	font-size: 18px;
 	transition: 0.25s ease;
+	text-transform: uppercase;
+	letter-spacing: 1px;
 
 	:hover {
 		background-color: #b18e72;
