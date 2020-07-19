25ee3f643a36eabc5d25f4b5f4b65205173340f5
rrhodeswebdev <ryan@ryanrhodes.io>
2020-07-18 21:45:48 -0400

    added SEO to missing pages

--- src/pages/education/index.js
+++ src/pages/education/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import StandardPage from '../../components/layouts/StandardPage';
+import SEO from '../../components/seo';
 
 const Education = () => {
 	return (
 		<StandardPage>
+			<SEO title="Education" />
 			<h1>Education Page</h1>
 		</StandardPage>
 	);
