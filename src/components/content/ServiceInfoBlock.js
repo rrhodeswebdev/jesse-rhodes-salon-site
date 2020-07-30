b9245d3c731dc66b38132ad3cd1f05234f893359
rrhodeswebdev <ryan@ryanrhodes.io>
2020-07-29 23:44:07 -0400

    created styling service page

---
+++ src/components/content/ServiceInfoBlock.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const Container = styled.div`
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-content: center;
+	margin-bottom: 40px;
+	width: 100%;
+	border-bottom: 1px solid #e0ddd7;
+	padding-bottom: 40px;
+`;
+
+const Title = styled.h2`
+	font-size: 36px;
+	font-weight: 300;
+	text-align: center;
+	margin: 0;
+	padding-bottom: 20px;
+`;
+
+const Price = styled.p`
+	font-size: 28px;
+	font-weight: 300;
+	text-align: center;
+	margin: 0;
+	padding: 10px;
+`;
+
+const Time = styled.p`
+	font-size: 14px;
+	font-weight: 300;
+	text-align: center;
+	margin: 0;
+	padding: 5px;
+`;
+
+const Description = styled.p`
+	font-size: 18px;
+	font-weight: 300;
+	text-align: center;
+	margin: 0;
+	padding: 10px;
+`;
+
+const ServiceInfoBlock = props => {
+	const { title, price, time, description } = props;
+	return (
+		<Container>
+			<Title>{title}</Title>
+			<Price>{price}</Price>
+			<Description>{description}</Description>
+			<Time>Est time: {time}</Time>
+		</Container>
+	);
+};
+
+ServiceInfoBlock.propTypes = {
+	title: PropTypes.string.isRequired,
+	price: PropTypes.string.isRequired,
+	time: PropTypes.string,
+	description: PropTypes.string.isRequired,
+};
+
+export default ServiceInfoBlock;
--- src/pages/services/styling.js
+++ src/pages/services/styling.js
@@ -1,10 +1,91 @@
 import React from 'react';
+import styled from 'styled-components';
+import { Link } from 'gatsby';
+import SEO from '../../components/seo';
+import StandardPage from '../../components/layouts/StandardPage';
+import SimplePageHeader from '../../components/content/SimplePageHeader';
+import ServiceInfoBlock from '../../components/content/ServiceInfoBlock';
+import { Button } from '../../components/elements/Button';
+
+const stylingData = [
+	{
+		id: 1,
+		title: 'Haircut',
+		price: '$40+',
+		description:
+			'Includes a luxury shampoo, custom haircut, blowdry and head finishing.',
+		time: '45 minutes',
+	},
+	{
+		id: 2,
+		title: 'Blowout',
+		price: '$30+',
+		description: 'Includes a luxury shampoo, blowout, and heat finishing.',
+		time: '30 minutes',
+	},
+	{
+		id: 3,
+		title: 'Conditioning Treatment',
+		price: '$15+',
+		description: 'Includes a custom moisture treatment.',
+		time: '15 minutes',
+	},
+	{
+		id: 4,
+		title: 'Smoothing System',
+		price: '$100/oz',
+		description:
+			'Rejuvenol smoothing system to help improve manageability, shine, and condition hair.',
+		time: '60 minutes',
+	},
+];
+
+const MainContainer = styled.section`
+	display: flex;
+	margin: 0 auto 40px auto;
+	padding: 0 40px;
+	flex-direction: column;
+	align-items: center;
+	max-width: 1200px;
+	width: 100%;
+`;
+
+const Disclaimer = styled.span`
+	font-size: 14px;
+	font-weight: 300;
+	font-style: italic;
+	display: flex;
+	justify-content: center;
+	margin-bottom: 40px;
+`;
 
 const Styling = () => {
 	return (
-		<div>
-			<h1>Styling Services Page</h1>
-		</div>
+		<StandardPage>
+			<SEO title="Styling Services" />
+			<SimplePageHeader
+				text="Styling"
+				image="https://via.placeholder.com/1200x675.png"
+			/>
+			<MainContainer>
+				{stylingData.map(style => (
+					<ServiceInfoBlock
+						key={style.id}
+						title={style.title}
+						price={style.price}
+						description={style.description}
+						time={style.time && style.time}
+					/>
+				))}
+				<Disclaimer>
+					*Additional time may result in additional charges to all
+					services listed above
+				</Disclaimer>
+				<Link to="/booking/appointment">
+					<Button>Appointments</Button>
+				</Link>
+			</MainContainer>
+		</StandardPage>
 	);
 };
 
